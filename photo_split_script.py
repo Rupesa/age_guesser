191001d5664c84e22f01118f9244d5604695e958
tomasfilipe7 <tomasfilipe7@ua.pt>
2021-06-28 19:32:17 +0100

    Convert to gray

--- photo_split_script.py
+++ photo_split_script.py
@@ -2,9 +2,13 @@
 
 import shutil
 import os
-    
-source_dir = '/Users/ruisantos/Desktop/ano4/aa/project2/heartbeat/photos/{}'
-target_dir = '/Users/ruisantos/Desktop/ano4/aa/project2/heartbeat/photos2/{}'
+import cv2
+
+# source_dir = '/Users/ruisantos/Desktop/ano4/aa/project2/heartbeat/photos/{}'
+# target_dir = '/Users/ruisantos/Desktop/ano4/aa/project2/heartbeat/photos2/{}'
+
+source_dir = '/home/tomasfilipe7/Desktop/Universidade/4_Ano/2_Semestre/AA/age_guesser/data/data/photos/{}'
+target_dir = '/home/tomasfilipe7/Desktop/Universidade/4_Ano/2_Semestre/AA/age_guesser/data/data/photos2/{}'
 
 image_id = 0
 for i in range(7, 100):
@@ -23,5 +27,9 @@ for i in range(7, 100):
 
     file_names = os.listdir(this_src_dir)
     for file_name in file_names:
-        if (os.path.getsize('/Users/ruisantos/Desktop/ano4/aa/project2/heartbeat/photos/'+ str(i) + "/" + file_name) > 334) & (file_name != '.DS_Store'):
-            shutil.copy(os.path.join(this_src_dir, file_name), this_dest_dir)
+        if (os.path.getsize(source_dir.format(str(i) + "/" + file_name)) > 334) & (file_name != '.DS_Store'):
+            # shutil.copy(os.path.join(this_src_dir, file_name), this_dest_dir)
+            image = cv2.imread(os.path.join(this_src_dir, file_name), 1)
+            imgray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            print(os.path.join(this_dest_dir, file_name))
+            cv2.imwrite(os.path.join(this_dest_dir, file_name), imgray)
